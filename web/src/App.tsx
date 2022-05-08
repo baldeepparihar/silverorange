72b389017fc4864e1b22130781f23ce31c9254fd
baldeepparihar <baldeep@deeptechdesign.com>
2022-05-08 14:51:50 -0700

    filtering repos on client side where repo.fork === false

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
 export function App() {
-  async function getRepos() {
-    axios
-      .get('http://localhost:4000/repos')
-      .then((res) => console.log('Response: ', res))
-      .catch((err) => console.log(err.response.data));
-  }
+  const [repos, setRepos] = useState([]);
 
-  getRepos();
+  useEffect(() => {
+    async function getRepos() {
+      await axios
+        .get('http://localhost:4000/repos')
+        .then((res) => {
+          // My attempt at defining type with TypeScript (getting a squiggly line even though docs say this is correct)
+          const filteredRepos = res.data.filter((repo: Repo{}) => repo.fork === false);
+          console.log(filteredRepos)
+        })
+        .catch((err) => console.log(err.response.data));
+    }
+    
+    getRepos();
+  });
 
   return (
     <div className="App">
